64eab65659126212b91de3480170fc7d52ec397e
xukp20 <xukp20@mails.tsinghua.edu.cn>
2024-10-23 12:00:23 +0800

    update: reformat the grade rules parsing code

--- main.py
+++ main.py
@@ -7,50 +7,11 @@ mids_path = os.path.join(base_path, "mids")
 grading_file = os.path.join(refs_path, "grading_rules.md")
 ref_answer_file = os.path.join(refs_path, "ref_answer.md")
 
-split_result = split(ref_answer_file, grading_file)
-
-to_parse = []
-problems = []
-pattern = """
-### Id
-{id}
-
-### Problem & Answer
-{problem}
-
-### Grading Rules
-{rules}
-"""
-
-for i, text in enumerate(split_result):
-    to_parse.append(pattern.format(id=text[0], problem=text[1], rules=text[2]))
-    problems.append({
-        "id": text[0],
-        "problem": text[1],
-        "raw_rules": text[2],
-    })
-    print(to_parse[i])
-
-
 # load key for parse_grading_rules from src.configs.keys.parse_grading_rules.txt
 key = open("src/configs/keys/parse_grading_rules.txt", "r").read().strip()
 
-# parse grading rules
-from src.api.dify_api import completion_messages
-for i, text in enumerate(to_parse):
-    inputs = {
-        "query": text,
-    }
-    response = completion_messages(inputs, key)
-    import json
-    print(json.loads(response.strip("```json").strip("```").replace("\\", "\\\\").replace("\\\\\\\\", "\\\\")))
-    problems[i]["rules"] = json.loads(response.strip("```json").strip("```").replace("\\", "\\\\").replace("\\\\\\\\", "\\\\"))
+output_path = os.path.join(mids_path, "problems1.json")
 
-# save problems
-# output to mids directory
-import json
-output_path = os.path.join(mids_path, "problems.json")
-with open(output_path, "w") as f:
-    json.dump(problems, f)
+from src.preprocess.parse_grading_rules import parse_grading_rules
 
-print(f"Problems saved to {output_path}")
+parse_grading_rules(ref_answer_file, grading_file, output_path, key)
--- src/preprocess/parse_grading_rules.py
+++ src/preprocess/parse_grading_rules.py
@@ -59,3 +59,50 @@ class ParsedProblemList:
     
     def __repr__(self):
         return self.__str__()
+    
+
+from src.preprocess.split import split
+from src.api.dify_api import completion_messages
+import json
+
+
+def parse_grading_rules(ref_answer_file, grading_file, output_path, app_key):
+    split_result = split(ref_answer_file, grading_file)
+    
+    to_parse = []
+    problems = []
+    pattern = """
+### Id
+{id}
+
+### Problem & Answer
+{problem}
+
+### Grading Rules
+{rules}
+"""
+
+    for i, text in enumerate(split_result):
+        to_parse.append(pattern.format(id=text[0], problem=text[1], rules=text[2]))
+        problems.append({
+            "id": text[0],
+            "problem": text[1],
+            "raw_rules": text[2],
+        })
+
+    # parse grading rules
+    for i, text in enumerate(to_parse):
+        inputs = {
+            "query": text,
+        }
+        response = completion_messages(inputs, app_key)
+        problems[i]["rules"] = json.loads(response.strip("```json").strip("```").replace("\\", "\\\\").replace("\\\\\\\\", "\\\\"))
+
+    # save problems
+    # output to mids directory
+    with open(output_path, "w") as f:
+        json.dump(problems, f, ensure_ascii=False, indent=4)
+
+    print(f"Problems saved to {output_path}")
+
+    return ParsedProblemList([ParsedProblem.from_dict(problem) for problem in problems])
