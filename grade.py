17076a1632c81195aab1fe1e90429c17177c1953
xukp20 <xukp20@mails.tsinghua.edu.cn>
2024-10-23 11:02:28 +0800

    update

--- grade.py
+++ grade.py
@@ -10,108 +10,25 @@ ref_answer_file = os.path.join(refs_path, "ref_answer.md")
 student_ans_dir = os.path.join(base_path, "raw_answers")
 student_file = "主观题 HW1.md"
 
-# split the student answer file
+# set the student file
 student_ans_file = os.path.join(student_ans_dir, student_file)
-# split_result = split_only_problem(student_ans_file)
 
 # load the grading rules
 parsed_path = os.path.join(mids_path, "problems.json")
-# with open(parsed_path, "r") as f:
-    # ref_problems = json.load(f)
-# translate to id: problem
-# ref_problems = {problem["id"]: problem for problem in ref_problems}
 
-# match the student answer with the grading rules
-# check_pairs = [
-#     {
-#         "ref": ref_problems[problem[0]],    # dict
-#         "student": problem[1]   # text
-#     }
-#     for problem in split_result
-# ]
-
-# check one by one
-
-# def format_rules_table(rules, start_id=1):
-#     """
-#         Map the list of rules to a md table:
-#         inputs are a list of dict with keys: id, rule, score
-#         | id | rule | score |
-#     """
-#     table = "| 规则序号 | 所属小题 | 评分规则 | 分值 |\n| --- | --- | --- | --- |\n"
-#     for i, rule in enumerate(rules):
-#         table += f"| {i+start_id} | {rule['id']} | {rule['rule']} | {rule['score']} |\n"
-#     return table
-
-# def format_rules_tables(rules):
-#     """
-#         One table for each rule, but different id.    
-#     """
-#     tables = []
-#     for i, rule in enumerate(rules):
-#         tables.append(format_rules_table([rule], start_id=i+1))
-#     return tables
-
-# input_pattern = """
-# ### 参考答案
-# {ref_problem}
-
-# ### 学生答案
-# {student_problem}
-
-# ### 评分标准
-# {rules_table}
-# """
-
-# for pair in check_pairs:
-
-#     # format the input
-#     # inputs = {
-#     #     "ref_problem": pair["ref"]["problem"],
-#     #     "student_problem": pair["student"],
-#     #     "rules_table": format_rules_table(pair["ref"]["rules"])
-#     # }
-
-#     # print(input_pattern.format(**inputs))
-
-#     for table in format_rules_tables(pair["ref"]["rules"]):
-#         inputs = {
-#             "ref_problem": pair["ref"]["problem"],
-#             "student_problem": pair["student"],
-#             "rules_table": table
-#         }
-
-#         print(input_pattern.format(**inputs))
-#         # print(table)
-#         print("\n\n\n")
-
-#     break
-
-
-from src.grading.check import check
+from src.grading.check import check, print_all_records_with_ref_scores, print_all_records
 
 key = "app-fKyyxey5TrMJg9Y7pWFindBX"
 
-all_results = check(parsed_path, student_ans_file, key)
+# all_results = check(parsed_path, student_ans_file, key)
 
-# print(all_results)
-
-import json
-json.dump(all_results, open("./results.json", "w"))
-
-# for result in all_results:
-#     print(result)
-#     print("\n\n\n")
+# import json
+# json.dump(all_results, open("./results.json", "w"), ensure_ascii=False, indent=4)
 
 # loads
 all_results = json.load(open("./results.json", "r"))
 
-for id, result in all_results.items():
-    print(id)
-    print(result["ref"])
-    print(result["student"])
-    for i in range(len(result["scores"])):
-        print(result["rules"][i])
-        print(result["scores"][i])
-        print(result["reasons"][i])
+md_report = print_all_records(all_results)
 
+with open("report.md", "w") as f:
+    f.write(md_report)
--- src/grading/check.py
+++ src/grading/check.py
@@ -18,6 +18,36 @@ def format_rules_table(rules, start_id=1):
         table += f"| {i+start_id} | {rule['id']} | {rule['rule']} | {rule['score']} |\n"
     return table
 
+def format_rules_table_with_scores(rules, scores, reasons=None, start_id=1):
+    """
+        Map the list of rules to a md table:
+        inputs are a list of dict with keys: id, rule, score
+        | id | rule | score | score |
+    """
+    if not reasons:
+        table = "| 规则序号 | 所属小题 | 评分规则 | 分值 | 得分 |\n| --- | --- | --- | --- | --- |\n"
+    else:
+        table = "| 规则序号 | 所属小题 | 评分规则 | 分值 | 得分 | 评分原因 |\n| --- | --- | --- | --- | --- | --- |\n"
+
+    for i, rule in enumerate(rules):
+        if not reasons:
+            table += f"| {i+start_id} | {rule['id']} | {rule['rule']} | {rule['score']} | {scores[i]} |\n"
+        else:
+            table += f"| {i+start_id} | {rule['id']} | {rule['rule']} | {rule['score']} | {scores[i]} | {reasons[i]} |\n"
+
+    return table
+
+def format_subproblem_table(agg_scores):
+    """
+        Map the dict of scores to a md table:
+        inputs are a dict with key id.
+        | id | score |
+    """
+    table = "| 小题序号 | 得分 |\n| --- | --- |\n"
+    for id, score in agg_scores.items():
+        table += f"| {id} | {score} |\n"
+    return table
+
 def format_rules_tables(rules):
     """
         One table for each rule, but different id.    
@@ -61,7 +91,40 @@ def check_one_rule(ref_problem, student_problem, rules_table, app_key):
     return process, scores
 
 
+# tool for agg rules for the same subproblem
+def group_scores(agg_scores, new_score, rule):
+    """
+        Agg the scores by the id of the corresponding rule.
+
+        Args:
+            agg_scores: dict with key id.
+            new_score: int
+            rule: dict with keys: id, rule, score
+        
+        Returns:
+            agg_scores: dict with key id.
+    """
+    rule_id = rule["id"]
+    if rule_id in agg_scores:
+        agg_scores[rule_id].append(new_score)
+    else:
+        agg_scores[rule_id] = [new_score]
+    return agg_scores
+
+
 def check_one_problem(ref_problem, student_problem, rules, app_key):
+    """
+        Check one problem with multiple rules.
+
+        Args:
+            ref_problem: text of the reference problem with answers
+            student_problem: text of the student's answer
+            rules: list of dict with keys: id, rule, score
+            app_key: key for the api
+        
+        Returns:
+            records: dict with keys: ref, student, rules, scores, reasons, agg_scores
+    """
     tables = format_rules_tables(rules)
     records = {
         "ref": ref_problem,
@@ -70,6 +133,7 @@ def check_one_problem(ref_problem, student_problem, rules, app_key):
     }
     all_scores = []
     all_reasons = []
+    agg_scores = {}
 
     for rule_id, table in enumerate(tables):
         max_score = rules[rule_id]["score"]
@@ -95,9 +159,12 @@ def check_one_problem(ref_problem, student_problem, rules, app_key):
             continue
         all_scores.append(scores[str(rule_id)])
         all_reasons.append(process)
+        agg_scores = group_scores(agg_scores, scores[str(rule_id)], rules[rule_id-1])
     
     records["scores"] = all_scores
     records["reasons"] = all_reasons
+    # sum the scores for each rule
+    records["agg_scores"] = {rule_id: sum(scores) for rule_id, scores in agg_scores.items()}
 
     return records
 
@@ -132,4 +199,44 @@ def check(refs_path, student_ans_file, app_key):
         all_records[id] = records
 
     return all_records
-        
+
+
+# tool
+def print_one_problem_record(records):
+    """
+        Print the record in a human-readable way.
+    """
+    string = ""
+    string += f"### Ref\n{records['ref']}\n"
+    string += f"### Student\n{records['student']}\n"
+    string += f"### Rules\n"
+    string += format_rules_table_with_scores(records["rules"], records["scores"], records["reasons"])
+    string += f"### Agg Scores\n"
+    string += format_subproblem_table(records["agg_scores"])
+    
+    return string
+
+
+def print_all_records(all_records):
+    """
+        Print the records in a human-readable way as a md table.
+    """
+    string = ""
+    for id, records in all_records.items():
+        string += f"## Problem {id}\n"
+        string += print_one_problem_record(records)
+        string += "\n"
+    return string
+
+def print_all_records_with_ref_scores(all_records, ref_scores):
+    """
+        Print the records in a human-readable way as a md table.
+    """
+    string = ""
+    for id, records in all_records.items():
+        string += f"## Problem {id}\n"
+        string += print_one_problem_record(records)
+        string += f"### Ref Scores\n"
+        string += format_subproblem_table(ref_scores[id])
+        string += "\n"
+    return string
